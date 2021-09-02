e9cdcb9a286657b2a14c3268bb1355dbfd2367b7
Polina <paulinnn@yandex.ru>
2021-09-02 20:26:58 +0300

    added slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__wpapper').slick({
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
+    });
+  });
+          
