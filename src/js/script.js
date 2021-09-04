ad427a438490d74e6a5bff716ac205d0a7f5b033
Polina <paulinnn@yandex.ru>
2021-09-04 20:40:30 +0300

    added tabs

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,20 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt="left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt="right"></button>',
     });
-  });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
+    });
+    
+    $('.catalog__link').each(function(i) {
+        $(this).on('click', function(e) {
+            $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+        })
+    });
+    
+});
+
+
           
