735af1ec68877d8bd9a087e881f0edce2d3c017b
Polina <paulinnn@yandex.ru>
2021-09-06 17:39:10 +0300

    added animation.css

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
+          .closest('div.container').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active animate__animated animate__bounceInLeft');
     });
     
     function toggleSlide(item) {
